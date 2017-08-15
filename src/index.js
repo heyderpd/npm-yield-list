9d2f85d2e3e690de60456998b5c3d1d27e74eaea
Heyder Dias <heyderpd@gmail.com>
2017-08-14 22:03:17 -0300

    v0.5.0

--- src/index.js
+++ src/index.js
@@ -1,1 +1,147 @@
-https://github.com/heyderpd/javascript-sandbox/commit/47cca9a8e44156cb81ad2f0490b6ba211420dee3
+const objectMap = (obj, fx, def = {}) => Object
+  .keys(obj).reduce(
+    (acc, key) => {
+      const value = obj[key]
+      return ({
+        ...acc,
+        [key]: value(fx)
+      })
+    },
+    def)
+
+const createObject = (state = {}, fxs = {}) => {
+  return objectMap(
+    fxs,
+    () => state,
+    state)
+}
+
+const listItem = (state, remove) => {
+  return createObject(
+    state,
+  { remove })
+}
+
+export default () => {
+  const state = {
+    lastKeys: 0,
+    length: 0,
+    first: {},
+    last: {},
+    links: {},
+    position: 0
+  }
+
+  const chainItem = (itemA, itemB) => {
+    itemA.next = itemB
+    itemB.before = itemA
+  }
+
+  const getFirst = () => state.first.next
+
+  const getLast = () => state.last.before
+
+  const setFirst = item => {
+    const first = getFirst()
+    chainItem(state.first, item)
+    chainItem(item, first)
+  }
+
+  const setLast = item => {
+    const last = getLast()
+    chainItem(last, item)
+    chainItem(item, state.last)
+  }
+
+  const ifFirstSet = item => {
+    if (state.length === 0) {
+      chainItem(state.first, item)
+      chainItem(item, state.last)
+      return true
+    }
+  }
+
+  const removeFromList = key => {
+    const item = state.links[key]
+    if (item) {
+      chainItem(
+        item.before,
+        item.next)
+      delete state.links[key]
+      state.length -= 1
+    }
+  }
+  const remove = state => () => removeFromList(state().key)
+
+  const push = obj => {
+    const item = listItem({
+        obj,
+        key: state.lastKeys
+      },
+      remove)
+
+    if (!ifFirstSet(item)) {
+      setLast(item)
+    }
+
+    state.lastKeys += 1
+    state.length += 1
+  }
+
+  const mapFrom = (first, next) => (fx = x=>x, circular = false) => {
+    if (circular) {
+      return circularMap(first, next, fx)
+    }
+
+    let nextItem = first    
+    let limit = state.length
+    const result = []
+    while (nextItem = next(nextItem)) {
+      if (limit-- < 0) {
+        throw 'infinit map! this is a Circular!'
+      }
+
+      const { obj } = nextItem
+      if (obj) {
+        result.push(fx(obj))
+      }
+    }
+    return result
+  }
+
+  const circularMap = function* (nextItem, next, fx) {
+    while (nextItem = next(nextItem)) {
+      const { obj } = nextItem
+      if (obj) {
+        yield fx(obj)
+      }
+    }
+  }
+  
+  const map = mapFrom(
+    state.first,
+    i => i.next)
+
+  const mapReverse = mapFrom(
+    state.last,
+    i => i.before)
+
+  const makeCircular = circular => {
+    const first = getFirst()
+    const last = getLast()
+    if (circular) {
+      chainItem(last, first)            
+    } else {
+      first.before = state.first      
+      last.next = state.last
+    }
+  }
+
+  return {
+    push,
+    map,
+    mapReverse,
+    makeCircular,
+    state
+  }
+}
---
+++ test/02.spec.js
@@ -0,0 +1,45 @@
+import assert from 'assert'
+
+import list from '../dist'
+
+const items = [
+  { a: 1 },
+  { b: 2 },
+  { c: 3 },
+]
+
+describe('list', function() {
+  it('push', () => {
+    const L = list()
+    L.push(items[0])
+    L.push(items[1])
+    L.push(items[2])
+    
+    const arr = L.map()
+
+    assert.deepEqual(
+      arr,
+      items)
+  })
+
+  it('circular', () => {
+    const L = list()
+    L.push(items[0])
+    L.push(items[1])
+    L.makeCircular(true)
+    
+    const iterator = L.map(
+      x=> x,
+      true)
+
+    assert.deepEqual(
+      iterator.next().value,
+      {a: 1})
+    assert.deepEqual(
+      iterator.next().value,
+      {b: 2})
+    assert.deepEqual(
+      iterator.next().value,
+      {a: 1})
+  })
+})
